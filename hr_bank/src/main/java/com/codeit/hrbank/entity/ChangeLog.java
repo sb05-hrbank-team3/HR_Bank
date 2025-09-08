1813039d385ef1d7a3668eac4520a1a77c69a793
Namsoo315 <gustn2130@naver.com>
2025-09-08 22:54:37 +0900

    feat: Backup, BinaryContent, ChangeLog, Department, Employee, History entity 생성 및 테이블 매핑

---
+++ hr_bank/src/main/java/com/codeit/hrbank/entity/ChangeLog.java
@@ -0,0 +1,50 @@
+package com.codeit.hrbank.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "change_logs")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChangeLog {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private ChangeLogType type;
+
+  @Column(name = "ip_address", nullable = false, length = 100)
+  private String ipAddress;
+
+  @Column(nullable = false, length = 100)
+  private String memo;
+
+  @Column(name = "at", nullable = false)
+  private Instant at;
+
+  @OneToMany(fetch = FetchType.LAZY)
+  @JoinColumn(name = "employee_id", nullable = false)
+  private List<Employee> employee;
+}
