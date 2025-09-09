4f667da1e2a8a509fa1d27c7a74ca3ff50428f4d
yelim-Lee <dpfla1078@naver.com>
2025-09-09 13:58:14 +0900

    refactor : 테이블명 histories 변경

--- hr_bank/src/main/java/com/codeit/hrbank/entity/History.java
+++ hr_bank/src/main/java/com/codeit/hrbank/entity/History.java
@@ -18,7 +18,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "historys")
+@Table(name = "histories")
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
