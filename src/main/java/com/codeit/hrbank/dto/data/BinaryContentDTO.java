1320fede3068bdd79f8f7c3faed245b99346cad4
jymin0 <alswodud119@naver.com>
2025-09-09 16:59:52 +0900

    feat : add BinarContent DTOs

---
+++ src/main/java/com/codeit/hrbank/dto/data/BinaryContentDTO.java
@@ -0,0 +1,5 @@
+package com.codeit.hrbank.dto.data;
+
+public record BinaryContentDTO(Long id, String name, Long size, String contentType) {
+
+}
---
+++ src/main/java/com/codeit/hrbank/dto/response/BinaryContentResponse.java
@@ -0,0 +1,5 @@
+package com.codeit.hrbank.dto.response;
+
+public record BinaryContentResponse(Long id, String name, Long size, String contentType) {
+
+}
