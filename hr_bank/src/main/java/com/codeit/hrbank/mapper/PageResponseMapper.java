5bf5c9cf6896d796c902051541c34991b6a0c565
Namsoo315 <gustn2130@naver.com>
2025-09-09 15:10:58 +0900

    feat: pageResponseMapper 추가 (Page사용)

---
+++ hr_bank/src/main/java/com/codeit/hrbank/mapper/PageResponseMapper.java
@@ -0,0 +1,19 @@
+package com.codeit.hrbank.mapper;
+
+import com.codeit.hrbank.dto.data.CursorPageResponse;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+@Mapper(componentModel = "spring")
+public interface PageResponseMapper {
+
+    // page 기반
+    default <T> CursorPageResponse<T> fromPage(Page<T> page) {
+        return CursorPageResponse.<T>builder()
+            .content(page.getContent())
+            .size(page.getSize())
+            .totalElements(page.getTotalElements())
+            .hasNext(page.hasNext())
+            .build();
+    }
+}
