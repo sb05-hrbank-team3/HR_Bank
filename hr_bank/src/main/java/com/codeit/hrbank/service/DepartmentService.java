ce61b0a1964911f8e378abe02871b0845c66ced3
Namsoo315 <gustn2130@naver.com>
2025-09-10 17:03:40 +0900

    docs: cursor 타입 변경 및 인터페이스 정리

--- hr_bank/src/main/java/com/codeit/hrbank/service/DepartmentService.java
+++ hr_bank/src/main/java/com/codeit/hrbank/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.codeit.hrbank.service;
 import com.codeit.hrbank.dto.data.DepartmentDTO;
 import com.codeit.hrbank.dto.request.DepartmentCreateRequest;
 import com.codeit.hrbank.dto.request.DepartmentUpdateRequest;
+import com.codeit.hrbank.dto.response.CursorPageResponse;
 import com.codeit.hrbank.entity.Department;
 import java.time.Instant;
 import java.util.List;
@@ -11,26 +12,20 @@ import java.util.Optional;
 public interface DepartmentService {
 
 
+  DepartmentDTO create(DepartmentCreateRequest request);
 
-
-  public DepartmentDTO create(DepartmentCreateRequest request);
-
-  public List<DepartmentDTO> findAll(String nameOrDescription,
+  CursorPageResponse<DepartmentDTO> findAll(String nameOrDescription,
       Long idAfter,
-      Instant cursor,
+      String cursor,
       int size,
       String sortField,
       String sortDirection);
 
-  public Optional<DepartmentDTO> findById(Long id);
+  Optional<DepartmentDTO> findById(Long id);
 
-  public void deleteById(Long id);
+  void deleteById(Long id);
 
-  public DepartmentDTO update(Long id,DepartmentUpdateRequest request);
-
-
-
-
+  DepartmentDTO update(Long id, DepartmentUpdateRequest request);
 
 
 }
