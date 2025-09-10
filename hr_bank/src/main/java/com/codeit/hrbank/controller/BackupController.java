5163e7ad182d25ae5761931f5ab8df64fc054425
bustam00 <bustam080@gmail.com>
2025-09-10 18:06:52 +0900

    feat: backup관련 기능 70%이상 구현 완료

--- hr_bank/src/main/java/com/codeit/hrbank/controller/BackupController.java
+++ hr_bank/src/main/java/com/codeit/hrbank/controller/BackupController.java
@@ -1,19 +1,27 @@
 package com.codeit.hrbank.controller;
 
 
+import com.codeit.hrbank.config.LocalDateToInstantDeserializer;
 import com.codeit.hrbank.dto.data.BackupDTO;
+import com.codeit.hrbank.entity.BackupStatus;
 import com.codeit.hrbank.entity.Employee;
+import com.codeit.hrbank.mapper.BackupMapper;
 import com.codeit.hrbank.service.BackupService;
 import com.codeit.hrbank.service.csv.CsvExportService;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,11 +31,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackupController {
   private final BackupService backupService;
 
-
   @GetMapping("")
+  public ResponseEntity<List<BackupDTO>> findAllBackups(
+      @RequestParam(required = false) String worker,
+      @RequestParam(required = false) BackupStatus status ,
+      @RequestParam(required = false) Instant startedAtFrom,
+      @RequestParam(required = false) Instant startedAtTo,
+      @RequestParam(required = false) Integer idAfter,
+      @RequestParam(required = false) String cursor,
+      @RequestParam(defaultValue = "10") int size,
+      @RequestParam(defaultValue = "startedAt") String sortField,
+      @RequestParam(defaultValue = "DESC") String sortDirection
+
+      ){
+    List<BackupDTO> backupDTOS= backupService.findAllBackups();
+
+
+    return ResponseEntity.ok(backupDTOS);
+  }
+
+
+
+  @PostMapping("")
   public ResponseEntity<BackupDTO> createBackup(HttpServletRequest request) throws IOException {
      BackupDTO backupDto = backupService.createBackup(request);
 
+
+    return ResponseEntity.status(HttpStatus.OK).body(backupDto);
+  }
+
+  @GetMapping("/latest")
+  public ResponseEntity<BackupDTO> findLatestBackup(
+      HttpServletRequest request,
+      @RequestParam(defaultValue = "COMPLETED",required = false)BackupStatus status
+      ) {
+    BackupDTO backupDto = backupService.findLatestBackup(status);
 
     return ResponseEntity.status(HttpStatus.OK).body(backupDto);
   }
--- hr_bank/src/main/java/com/codeit/hrbank/repository/impl/BackupQueryRepositoryImpl.java
+++ hr_bank/src/main/java/com/codeit/hrbank/repository/impl/BackupQueryRepositoryImpl.java
@@ -1,12 +1,127 @@
 package com.codeit.hrbank.repository.impl;
 
 
+import com.codeit.hrbank.entity.Backup;
+import com.codeit.hrbank.entity.BackupStatus;
+import com.codeit.hrbank.entity.QBackup;
+import com.codeit.hrbank.entity.QChangeLog;
+import com.codeit.hrbank.repository.BackupQueryRepository;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.Instant;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class BackupQueryRepositoryImpl {
+public class BackupQueryRepositoryImpl implements BackupQueryRepository {
+  private final JPAQueryFactory queryFactory;
+  private static QBackup backup  = QBackup.backup;
+
+  @Override
+  public List<Backup> findAllBackups(
+      String worker, BackupStatus status, Instant startedAtFrom, Instant startedAtTo
+      , int idAfter, String cursor, int size, String sortField, String sortDirection
+  ) {
+    BooleanBuilder where = new BooleanBuilder();
+
+    if(worker != null && !worker.isBlank()) {
+      where.and(backup.worker.containsIgnoreCase(worker));
+    }
+
+    if(status != null) {
+      where.and(backup.status.eq(status));
+    }
+
+    if(startedAtFrom != null) {
+      where.and(backup.startedAt.goe(startedAtFrom));
+    }
+
+    if(startedAtTo != null) {
+      where.and(backup.endedAt.loe(startedAtTo));
+    }
+
+    if(sortField.equals("startedAt")) {
+      if(sortDirection.equals("ASC")) {
+        return queryFactory
+            .select(backup)
+            .from(backup)
+            .where(where)
+            .limit(size)
+            .orderBy(backup.startedAt.asc())
+            .fetch();
+      }
+      else if(sortDirection.equals("DESC")) {
+        return queryFactory
+            .select(backup)
+            .from(backup)
+            .where(where)
+            .limit(size)
+            .orderBy(backup.startedAt.desc())
+            .fetch();
+
+      }
+    }
+
+    if(sortField.equals("endedAt")) {
+      if(sortDirection.equals("ASC")) {
+        return queryFactory
+            .select(backup)
+            .from(backup)
+            .where(where)
+            .limit(size)
+            .orderBy(backup.endedAt.asc())
+            .fetch();
 
 
+      }
+      else if(sortDirection.equals("DESC")) {
+        return queryFactory
+            .select(backup)
+            .from(backup)
+            .where(where)
+            .limit(size)
+            .orderBy(backup.endedAt.desc())
+            .fetch();
+
+      }
+    }
+
+    if(sortField.equals("status")) {
+      if(sortDirection.equals("ASC")) {
+        return queryFactory
+            .select(backup)
+            .from(backup)
+            .where(where)
+            .limit(size)
+            .orderBy(backup.status.asc())
+            .fetch();
+
+      }
+    }
+    return queryFactory
+        .select(backup)
+        .from(backup)
+        .where(where)
+        .limit(size)
+        .orderBy(backup.status.desc())
+        .fetch();
+
+  }
+
+  @Override
+  public Backup getBackupLatest(BackupStatus status) {
+
+    BooleanBuilder where = new BooleanBuilder();
+    where.and( backup.status.eq(status) );
+
+    return queryFactory
+        .select(backup)
+        .from(backup)
+        .where(where)
+        .orderBy(backup.endedAt.desc())
+        .fetchFirst();
+
+  }
 }
