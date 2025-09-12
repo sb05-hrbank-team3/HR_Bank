2ef914ad51d4d7480dcdf68c4058cd8805c4f1e2
Namsoo315 <gustn2130@naver.com>
2025-09-12 16:29:07 +0900

    docs: Swagger 설정 변경

--- hr_bank/src/main/java/com/codeit/hrbank/config/OpenApiConfig.java
+++ hr_bank/src/main/java/com/codeit/hrbank/config/OpenApiConfig.java
@@ -1,25 +1,27 @@
 package com.codeit.hrbank.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new io.swagger.v3.oas.models.info.Info()
-                        .title("API 문서")
-                        .version("v1.0.0")
-                        .description("Swagger API 문서입니다."))
-                .servers(java.util.List.of(
-                        new io.swagger.v3.oas.models.servers.Server()
-                                .url("http://localhost:8080")
-                                .description("로컬 서버")
-                ));
-    }
+  // 개발 환경에 맞춰서 바꿈/
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new io.swagger.v3.oas.models.info.Info()
+            .title("API 문서")
+            .version("v1.0.0")
+            .description("Swagger API 문서입니다."))
+        .servers(List.of(
+            new io.swagger.v3.oas.models.servers.Server()
+                .url("https://hrbank-production.up.railway.app/")
+                .description("프로덕션 서버")
+        ));
+  }
+
 
 }
